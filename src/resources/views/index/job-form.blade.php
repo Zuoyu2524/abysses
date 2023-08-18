c94ee3043163312e746890d71cfecf5ad60a96b7
Zuoyu Zhang <129968070+Zuoyu2524@users.noreply.github.com>
2023-08-18 07:48:26 -0400

    Update job-form.blade.php

--- src/resources/views/index/job-form.blade.php
+++ src/resources/views/index/job-form.blade.php
@@ -1,6 +1,6 @@
 <h3>Create a new Abysses job <a class="btn btn-default btn-xs pull-right" href="{{ route('manual-tutorials', ['abysses', 'about']) }}" title="More information on Abysses" target="_blank"><i class="fas fa-info-circle"></i></a></h3>
 <p>
-    You can choose to directly test the images using the default parameters to get the labels. Or manually annotate some of the images, again train the model and then test to get the image labels. The annotated labels in retrain task are very important, so please be careful to set the labels. Here we expect you to use the training function again to help us improve the performance of the network after using the test function to change the incorrect label results. Thank you very much for your correction.
+    This abysses function help you to classify the images using the CNN Neural Networks. Please click on the pull-down menu to select "classify" and then click on the button "Start Classify" to create your job.
 </p>
 <form id="abysses-job-form" method="POST" action="{{ url("api/v1/volumes/{$volume->id}/abysses-jobs") }}" v-on:submit="submit">
     @csrf
@@ -9,8 +9,7 @@
         <legend>Select working mode</legend>
         <select id="operation" name="operation" v-model="typeValue">
             <option value="">Please select a working mode</option>
-            <option value="test">Test</option>
-            <option value="train">Train</option>
+            <option value="test">Classify</option>
         </select>
         <div class="form-group{{ $errors->has('volume') ? ' has-error' : '' }}">
             @if($errors->has('volume'))
@@ -23,8 +22,7 @@
                     </div>
                 </div>
             @else
-                <button type="submit" id="testButton" class="btn btn-success pull-right" :disabled="canSubmit || !typeValue || typeValue === 'train'" v-show="typeValue && typeValue !== 'train'">Start Testing</button>
-                <button type="submit" id="trainButton" class="btn btn-success pull-right" :disabled="canSubmit || !typeValue || typeValue === 'test'" v-show="typeValue && typeValue !== 'test'">Start Training</button>
+                <button type="submit" id="testButton" class="btn btn-success pull-right" :disabled="canSubmit || !typeValue || typeValue === 'train'" v-show="typeValue && typeValue !== 'train'">Start Classify</button>            
             @endif
         </div>
         <div></div>
@@ -55,6 +53,3 @@
     biigle.$declare('abysses.hasErrors', {!! $errors->any() ? 'true' : 'false' !!});
 </script>
 @endpush
-
-
-
