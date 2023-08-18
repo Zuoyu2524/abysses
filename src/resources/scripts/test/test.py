2ad1a9814e3abad89a964cb4ca4b4f651bc53120
Zuoyu Zhang <129968070+Zuoyu2524@users.noreply.github.com>
2023-08-18 07:49:35 -0400

    Update test.py

--- src/resources/scripts/test/test.py
+++ src/resources/scripts/test/test.py
@@ -1,106 +1,59 @@
-"""import numpy as np
-import os
-import json
-import sys
-from PIL import Image
-from concurrent.futures import ThreadPoolExecutor, wait
-from concurrent.futures import ProcessPoolExecutor, as_completed
-from ImageCollection import ImageCollection
-
-
-class DetectionRunner(object):
-
-    def __init__(self, params):
-        # Dict of image IDs and file paths to the images to process.
-        self.images = params['images']
-        # Path to the directory to store temporary files.
-        self.tmp_dir = params['tmp_dir']
-
-    def postprocess_map(self, image, threshold):
-        saliency_map = np.load(self.image_path(image, 'npy'))
-        binary_map = np.where(saliency_map > threshold, 255, 0).astype(np.uint8)
-        mask = np.zeros_like(binary_map)
-        points = []
-
-        with open(self.image_path(image, 'json'), 'w') as outfile:
-            json.dump(points, outfile)
-        os.remove(self.image_path(image, 'npy'))
-
-    def process_cluster(self, cluster):
-
-        for i, image in enumerate(cluster):
-            np.save(self.image_path(image, 'npy'), image)
-
-        return 0
-
-    def run(self):
-        full_executor = ThreadPoolExecutor(max_workers=1)
-
-        images = ImageCollection(self.images, executor=full_executor)
-        images.prune_corrupt_images()
-        total_images = len(images)
-
-        if total_images == 0:
-            print('No image files to process.')
-        
-        clusters = [images]
-
-        jobs = []
-
-        post_executor = full_executor
-
-        for i, cluster in enumerate(clusters):
-            threshold = self.process_cluster(cluster)
-            jobs.extend([post_executor.submit(self.postprocess_map, image, threshold) for image in cluster])
-
-        wait(jobs)
-
-
-    def image_path(self, image, suffix):
-        return '{}/{}.{}'.format(self.tmp_dir, image.id, suffix)
-
-
-with open(sys.argv[1]) as f:
-    params = json.load(f)
-
-
-
-runner = DetectionRunner(params)
-runner.run()"""
-
 import logging
 from concurrent.futures import ThreadPoolExecutor, wait
 import json
 import sys
-import tensorflow as tf
+import os
+from PIL import Image as PilImage
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+
+def pil_image(path):
+        image = PilImage.open(path)
+        if image.mode == 'RGBA':
+            image = image.convert('RGB')
+        elif image.mode == 'LA':
+            image = image.convert('L')
+        image = np.array(image)
+        image.resize(1,250)
+        return image
 
 def run():
-    # Configure logging
-    #log_file = 'log.txt'
-    #logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-    full_executor = ThreadPoolExecutor(max_workers=1)
+    full_executor = ThreadPoolExecutor(max_workers=2)
     try:
         with open(sys.argv[1]) as f:
             params = json.load(f)
 
         images = params['images']
-        labels = []
 
         tmp_dir = params['tmp_dir']
-        jobs = []
 
-        print(images)
-        for i, image in enumerate(images):
-            jobs.extend([full_executor.submit(image)])
-            wait(jobs)
-            image_path = '{}/{}.{}'.format(tmp_dir, image, 'json')
-            print(type(image))
-            labels = [int(image), 'dogs']
+        i = 1
+        for key, value in images.items():
+            print(i)
+            if(i == 1):
+                x = pil_image(images[key])
+                i = 0
+            else:
+                image = pil_image(images[key])
+                x = np.vstack((x, image))
+        print(x.shape)
+        cats = np.zeros(21)
+        dogs = np.ones(21)
+        labels = np.concatenate((cats, dogs))
+        model = MLPClassifier(hidden_layer_sizes=(100,),max_iter=100, random_state=42)
+        model.fit(x,labels)
+
+        for key, value in images.items():
+            x = pil_image(images[key])
+            y_pred = model.predict(x)
+            image_path = '{}/{}.{}'.format(tmp_dir, key, 'json')
+            if(y_pred == 0):
+                labels = [int(key), 'cat']
+            else:
+                labels = [int(key), 'dog']
             with open(image_path, 'w') as outfile:
                 json.dump(labels, outfile)
         print("success")
-        #logging.info("Success")  # Log success message
         return "success"
 
     except Exception as e:
